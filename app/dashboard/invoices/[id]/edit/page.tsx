3a555d1593ce261e55f789ddaa97b1f76e8c70cd
Andi Rainer <andreasr@hilarion5.com>
2025-01-20 12:35:20 +0100

    Fixing bugs

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -3,7 +3,7 @@ import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import Form from "@/app/ui/invoices/edit-form";
 import { notFound } from "next/navigation";
 
-export default async function Page(props: { params: { id: Promise<string> } }) {
+export default async function Page(props: { params: Promise<{id: string}> }) {
   const params = await props.params;
   const id = await params.id;
 
