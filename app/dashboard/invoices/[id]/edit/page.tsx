2caca4f331cabcfb2e5126ef43178f22e9a916ce
Andi Rainer <andreasr@hilarion5.com>
2025-01-20 12:24:24 +0100

    Functioning app

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,32 @@
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import Form from "@/app/ui/invoices/edit-form";
+import { notFound } from "next/navigation";
+
+export default async function Page(props: { params: { id: Promise<string> } }) {
+  const params = await props.params;
+  const id = await params.id;
+
+  const [invoice, customers] = await Promise.all([fetchInvoiceById(id), fetchCustomers()]);
+
+  if (!invoice) {
+    notFound();
+  }
+
+  const breadcrumbs = [
+    {
+      label: 'Invoices',
+      href: '/dashboard/invoices'
+    },
+    {
+      label: 'Edit Invoice',
+      href: `/dashboard/invoices/${id}/edit`, active: true
+    }];
+
+  return (
+    <main>
+      <Breadcrumbs breadcrumbs={breadcrumbs} />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
